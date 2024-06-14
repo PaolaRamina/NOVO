7ff35eefdffe29d57fdc6c2a292501c10170be7e
Paola Ramina <paolaramina153@gmail.com>
2024-06-14 19:56:14 -0300

    Add files via upload

---
+++ Entrada da Página.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd  
+import matplotlib.pyplot as plt
+import ipeadatapy as ip
+
+st.set_page_config(
+page_title="Lista de Exercícios 4",
+page_icon="🍰", 
+)
+
+
+st.header("Bem-vindo à nossa nova criação!") 
+
+"---"
+
+st.subheader("Viramos garotas de programa! 😉")
+
+st.write("Ana")
+st.write("Paola")
